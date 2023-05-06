178908a3ebb6521d57a1810041849ba1519813da
jh-lee99 <jun99h1219@gmail.com>
2023-05-06 23:29:07 +0900

    로그인 헤더 적용 및 세션만료시 로그아웃 기능 작성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import axios from "axios";
 import { useState, useEffect } from 'react';
 import Cookies from 'js-cookie';
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Travel from "./pages/Travel";
 import RegisterUpdate from "./pages/RegisterUpdate";
 
 const App = () => {
-  const [isLogin, setIsLogin] = useState(false);
-  const [user, setUser] = useState({});
-  const [accessToken, setAccessToken] = useState('');
+  const navigate = useNavigate();
   
-  useEffect(() => {
-    const accessToken = Cookies.get('accessToken');
-    if (accessToken) {
-      setAccessToken(accessToken);
-    }
-  }, []);
-
   // 모든 axios 요청 생성에서 전역적으로 사용되는 인터셉트.
   // 
   axios.interceptors.request.use(
@@ -40,39 +31,48 @@ const App = () => {
         });
           const data = await response.json();
           console.log(data.message);
-        // 아직 유효하면 200을 반환함
-        if (response.ok) {
-          console.log("Access token is still valid");
-        } else {
-          console.log("Access token is expired");
-          Cookies.remove("accessToken");
-          accessToken = Cookies.get('accessToken');
+          // 아직 유효하면 200을 반환함
+          if (response.ok) {
+            console.log("Access token is still valid");
+          } else {
+            console.log("Access token is expired");
+            Cookies.remove("accessToken");
+            accessToken = Cookies.get('accessToken');
           console.log("accessToken removed");
-          if (refreshToken && !accessToken) {
-            try {
-              console.log("access refresh Token.");
-              const response = await fetch('http://localhost:3000/api/token/refresh', {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ refreshToken }),
-              });
-              if (response.ok) {
-                const { accessToken: newAccessToken } = await response.json();
-                Cookies.set('accessToken', newAccessToken);
-                config.headers.Authorization = `Bearer ${newAccessToken}`;
-              } else {
-                throw new Error('토큰이 없습니다.');
-              }
-            } catch (error) {
-              console.error(error);
-            } finally {
-              console.log('accessToken: ', Cookies.get(accessToken));
+        }
+        if (!accessToken) {
+          try {
+            console.log("access refresh Token.");
+            const response = await fetch('http://localhost:3000/api/token/refresh', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ refreshToken }),
+            });
+            if (response.ok) {
+              const { accessToken: newAccessToken } = await response.json();
+              Cookies.set('accessToken', newAccessToken);
+              config.headers.Authorization = `Bearer ${newAccessToken}`;
+            } else {
+              throw new Error('토큰이 없습니다.');
             }
+          } catch (error) {
+            Cookies.remove("refreshToken");
+            if(Cookies.get("username")) {
+              Cookies.remove("username");
+              alert("세션이 끊겼습니다.\n로그아웃합니다.");
+              navigate("/", { replace: true });
+            }
+            console.error(error);
+          } finally {
+            console.log('accessToken: ', Cookies.get("accessToken"));
           }
         }
       } catch (error) {
+        Cookies.remove("refreshToken");
+        Cookies.remove("username");
+        console.error("쿠키를 전부 삭제함", error);
         console.error('Failed to check access token', error);
       }
       return config;
--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -10,27 +10,11 @@ const Header = () => {
   // axios 요청으로 username을 반환하는 요청을 보냄
   // (로그인 상태라면)인터셉터 통해서 토큰 갱신하고 유저 이름 받아옴(navbar에 출력)
   // (로그인 상태가 아니라면)로그인 회원가입 버튼 인터페이스 제공
-  const getusername = () => {
-    axios.get("http://localhost:3000/userinfo")
-    .then(res => {
-      if(res.status === 200)
-        return res.data.username;
-      else if (res.status === 401)
-        return "";
-      else return ""
-    })
-    .catch(err => {
-      console.log(err);
-      return "" 
-    });  
-  }
 
   const [RegistModalOn, setRegistModalOn] = useState(false);
   const [LoginModalOn, setLoginModalOn] = useState(false);
   const token = Cookies.get("accessToken");
-  Cookies.set("username", getusername());
   const username = Cookies.get("username");
-
 
   if (username && token) {
     console.log("have", username);
@@ -100,7 +84,6 @@ const Header = () => {
                 </Nav>
               </Navbar.Collapse>
             </Container>
-            <Dropdown />
           </Navbar>
         </header>
       </>
